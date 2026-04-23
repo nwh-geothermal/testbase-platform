b503c03cc5bbb17f239a3ce3f7d5d2e931e537f0
nwh-geothermal <wanghanyu@nwh.cn>
2026-04-23 09:45:38 +0800

    fix: remove image strip

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,11 @@ import { Hero } from '@/components/hero'
 import { Features } from '@/components/features'
 import { Cooperation } from '@/components/cooperation'
 import { Achievements } from '@/components/achievements'
-import { ImageStrip } from '@/components/image-strip'
-import { getAssetPath } from '@/lib/utils'
 
 export default function Home() {
   return (
     <div className='min-h-screen'>
       <Hero />
-      <ImageStrip
-        images={[
-          getAssetPath('/sim1.jpg'),
-          getAssetPath('/process1.jpg'),
-          getAssetPath('/method1.png')
-        ]}
-        className='py-12'
-      />
       <Features />
       <Achievements />
       <Cooperation />
