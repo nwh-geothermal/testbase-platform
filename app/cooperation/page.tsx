97cfbfc8bac7cd36dd625f948caf0e0be56fae32
nwh-geothermal <wanghanyu@nwh.cn>
2026-04-29 08:55:41 +0800

    fix: remove image strip in 产学研合作 page

--- app/cooperation/page.tsx
+++ app/cooperation/page.tsx
@@ -1,19 +1,9 @@
 import { CooperationForm } from '@/components/cooperation-form'
-import { ImageStrip } from '@/components/image-strip'
-import { getAssetPath } from '@/lib/utils'
 
 export default function CooperationPage() {
   return (
     <div className='min-h-screen bg-gray-50'>
       <CooperationForm />
-      <ImageStrip
-        images={[
-          getAssetPath('/meeting1.jpg'),
-          getAssetPath('/meeting2.jpg'),
-          getAssetPath('/chd.jpg')
-        ]}
-        className='pt-6 pb-16'
-      />
     </div>
   )
 }
